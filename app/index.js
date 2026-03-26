5cb683a6f9d15770a7c8fea276f00419e7b53fd6
Omkar Jagtap <omkarjagtap8443@gmail.com>
2026-03-26 15:40:19 +0530

    testing cicd for devlopment and testing

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working 🚀");
+  res.send("CI/CD Pipeline Working  testing it 🚀");
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
